e072ea2a50fb0a759dfd9ed9436a8fb09c18bc38
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2020-01-24 18:22:39 -0500

    finished robot sort

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -1,34 +1,18 @@
 class SortingRobot:
     def __init__(self, l):
-        """
-        SortingRobot takes a list and sorts it.
-        """
-        self._list = l          # The list the robot is tasked with sorting
-        self._item = None       # The item the robot is holding
-        self._position = 0      # The list position the robot is at
-        self._light = "OFF"     # The state of the robot's light
-        self._time = 0          # A time counter (stretch)
+        self._list = l
+        self._item = None
+        self._position = 0
+        self._light = "OFF"
+        self._time = 0
 
     def can_move_right(self):
-        """
-        Returns True if the robot can move right or False if it's
-        at the end of the list.
-        """
         return self._position < len(self._list) - 1
 
     def can_move_left(self):
-        """
-        Returns True if the robot can move left or False if it's
-        at the start of the list.
-        """
         return self._position > 0
 
     def move_right(self):
-        """
-        If the robot can move to the right, it moves to the right and
-        returns True. Otherwise, it stays in place and returns False.
-        This will increment the time counter by 1.
-        """
         self._time += 1
         if self._position < len(self._list) - 1:
             self._position += 1
@@ -37,11 +21,6 @@ class SortingRobot:
             return False
 
     def move_left(self):
-        """
-        If the robot can move to the left, it moves to the left and
-        returns True. Otherwise, it stays in place and returns False.
-        This will increment the time counter by 1.
-        """
         self._time += 1
         if self._position > 0:
             self._position -= 1
@@ -50,23 +29,10 @@ class SortingRobot:
             return False
 
     def swap_item(self):
-        """
-        The robot swaps its currently held item with the list item in front
-        of it.
-        This will increment the time counter by 1.
-        """
         self._time += 1
-        # Swap the held item with the list item at the robot's position
         self._item, self._list[self._position] = self._list[self._position], self._item
 
     def compare_item(self):
-        """
-        Compare the held item with the item in front of the robot:
-        If the held item's value is greater, return 1.
-        If the held item's value is less, return -1.
-        If the held item's value is equal, return 0.
-        If either item is None, return None.
-        """
         if self._item is None or self._list[self._position] is None:
             return None
         elif self._item > self._list[self._position]:
@@ -77,69 +43,63 @@ class SortingRobot:
             return 0
 
     def set_light_on(self):
-        """
-        Turn on the robot's light
-        """
         self._light = "ON"
+        
     def set_light_off(self):
-        """
-        Turn off the robot's light
-        """
         self._light = "OFF"
+        
     def light_is_on(self):
-        """
-        Returns True if the robot's light is on and False otherwise.
-        """
         return self._light == "ON"
 
     def sort(self):
-        """
-        Sort the robot's list.
-        """
-        # Fill this out
-        for i in range(0, len(l)):
-        	# we compare the item we are hold, if we are holding None, we need to grab the item at the current position
-        	self.compare_item()
-        	self.swap_item()
-	        # We need to check if we can move right first, if we can...
-	        if self.can_move_right() == True:
-		        # we move to the right
-		        self.move_right()
-	        # if we cannot... we want to set the current position of the robot back to the beggining
-	        else:
-	        	self._position = 0
-	        # we need to compare the item we are holding with our current positions item
-	        if self.compare_item() == 1:
-	        	if self.can_move_right() == True:
-	        		self.move_right()
-	        else:
-				if self.can_move_right() == True:
-					self.move_right()
-				else:
-					self._position = 0
-	        self._position += self.compare_item()
-	        self.swap_item()
-	        print('Can we move right?', self.can_move_right())
-	        self.move_right()
-	        # we check if the item we are holding is greater than the item at the current position
-	        self.compare_item()
-	        # if it is, we swap item with we are holding with item in current position
-	        self.swap_item()
-	        # else we want to continue and check if we can move right..
-	        # if we can, we move right..
-	        print('Can we move right?', self.can_move_right())
-	        self.move_right()
-	        print('Can we move right?', self.can_move_right())
-	        print('item we are holding', self._item)
-	        print('current position', self._position)
-	        print('compare item', self.compare_item())
-
-
+        # while light is off...
+        while self.light_is_on() == False:
+            # turn light on
+            self.set_light_on()
+            # while you can move right...
+            while self.can_move_right() == True:
+                # pick the item
+                self.swap_item()
+                # move right
+                self.move_right()
+                # if item is picked up is greater than 0
+                if self.compare_item() > 0:
+                    # drop what we have in position
+                    self.swap_item()
+                    # turn light off
+                    self.set_light_off()
+                # we move left instead
+                self.move_left()
+                # drop we have in position
+                self.swap_item()
+                # we move right
+                self.move_right()
+            # if light is on
+            if self._light == 'ON':
+                # break
+                break
+            # while you can move left...
+            while self.can_move_left() == True:
+                # pick up item
+                self.swap_item()
+                # move left
+                self.move_left()
+                # if item is less than 0...
+                if self.compare_item() < 0:
+                    # drop item in the same position
+                    self.swap_item()
+                    # turn light off
+                    self.set_light_off()
+                # we move right instead
+                self.move_right()
+                # drop item in position
+                self.swap_item()
+                # we move left
+                self.move_left()
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
-    	# v   v
     l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
 
     robot = SortingRobot(l)
